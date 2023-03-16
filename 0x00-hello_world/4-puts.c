36cc7f59369e49d252e1f855fae9751ec95e3ae1
hassan <compte.personnel975@gmail.com>
2023-03-16 05:56:41 -0700

    puts in place of printf #do hard things

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/* This program prints 'shit' to the console */
 int main(void) 
 {
 char strng[] = "\"Programming is like building a multilingual puzzle";
-
-puts(strng);
-  
+puts(strng);  
 return (0); 
 }
