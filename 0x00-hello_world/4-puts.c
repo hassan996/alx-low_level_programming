777176b9a9c183ff4e327f7995e05cff64c53eb3
hassan <compte.personnel975@gmail.com>
2023-03-16 08:13:45 -0700

    puts replace printf (respect to betty )

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void) 
 {
-char strng[] = "\"Programming is like building a multilingual puzzle";
-puts(strng);  
-return (0); 
+	char strng[] = "\"Programming is like building a multilingual puzzle";
+	puts(strng);  
+	return (0); 
 }
