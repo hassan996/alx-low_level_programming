3c9f4b85f996a9cff73e3b2b82859d0bed943829
hassan <compte.personnel975@gmail.com>
2023-03-16 08:18:21 -0700

    puts replace printf (respect to betty )

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,9 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
-	char strng[] = "\"Programming is like building a multilingual puzzle";
-	puts(strng);  
-	return (0); 
+	char tring[] = "\"Programming is like building a multilingual puzzle";
+	puts(tring);
+	return(0);
 }
