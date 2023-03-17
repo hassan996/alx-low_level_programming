49a3703b49343a8df6f6acfdd3a010abde02505a
hassan <compte.personnel975@gmail.com>
2023-03-17 16:01:54 -0700

    do it

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,11 @@ int main(void)
 	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		 if(num <= 8 )
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
 	}
 	putchar('\n');
 	return (0);
