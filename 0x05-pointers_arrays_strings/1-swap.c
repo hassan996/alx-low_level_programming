8f6b7dab9d7b9cac20b83c07b5e573f4dc49b8eb
hassan <compte.personnel975@gmail.com>
2023-03-27 17:18:25 -0700

    pointers and arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
-
 /**
- * swap_int - function swap a to b 
- * @a: first variable 
- * @b: secode variable
+ * swap_int - function swap a to b.
+ * @a: first variable.
+ * @b: secode variable.
  */
 void swap_int(int *a, int *b)
 {
 	int swap = *a;
+
 	*a = *b;
 	*b = swap;
 }
