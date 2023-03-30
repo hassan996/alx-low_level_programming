2c32249b4aab77e3f233d1b75a62e1221e09a015
hassan <compte.personnel975@gmail.com>
2023-03-30 09:23:06 -0700

    ready to push

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,9 +5,7 @@
  * @s1: pointer to the first string to be compared.
  * @s2: pointer to the second string to be compared.
  *
- * Return: If str1 < str2, the negative difference of the first unmatched characters.
- *         If str1 == str2, 0.
- *         If str1 > str2, the positive difference of the first unmatched characters.
+ * Return: return result
  */
 int _strcmp(char *s1, char *s2)
 {
