68d496fa1f66135fb2429f35b06d4e68e40205f6
hassan <compte.personnel975@gmail.com>
2023-03-30 09:41:06 -0700

    push

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,13 +2,13 @@
 #include "main.h"
 
 /**
- * strcat - function
+ * *_strcat - function
  * @dest: pointer to the string to be concatenated upon.
  * @src:  source string to be appended to @dest.
  *
  * Return: A pointer to the destination string @dest.
  */
-char *strcat(char *dest, const char *src)
+char *_strcat(char *dest, const char *src)
 {
 	int i = 0, dest_len = 0;
 
