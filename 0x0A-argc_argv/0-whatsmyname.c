fd52223d3b1b33b279355b7cb5b1353789d265ad
hassan <compte.personnel975@gmail.com>
2023-04-10 07:22:05 -0700

    print name programme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - this fun prints its name, followed by a new line
  * @argc: count line command argument
@@ -7,6 +8,7 @@
  */
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s \n", *argv);
+	printf("%s\n", *argv);
+
 	return (0);
 }
