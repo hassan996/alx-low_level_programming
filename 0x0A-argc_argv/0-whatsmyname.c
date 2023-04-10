7f78a3bad022dec62575968beb68c0a042f11c0e
hassan <compte.personnel975@gmail.com>
2023-04-10 07:18:57 -0700

    print name programme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * @argv:contain arg in line command
  * Return: 0
  */
-int main(int __attribute__((__unused__))argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s \n", *argv);
 	return (0);
