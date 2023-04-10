cd40423f93157485c10a72cebce7625764e1f732
hassan <compte.personnel975@gmail.com>
2023-04-10 08:21:16 -0700

    multiple passed by arg

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function multiple two num
+ * @argc:count number of arg in line cmd
+ * @argv:array of pointer to arg
+ * Return:1 if arg < 1
+ */
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc < 3)
+	{
+		printf("Errore\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		printf("%d\n", a * b);
+	}
+	return (0);
+}
+
