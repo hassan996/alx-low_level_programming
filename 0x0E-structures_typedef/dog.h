2e9036991d67f8d05b10714db5da8140f5a7b9d0
hassan996 <compte.personnel975@gmail.com>
2023-04-17 10:42:44 -0700

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,8 +18,8 @@ struct dog
 /**
  * dog_t - is new type of struct dog
  */
-
 typedef struct dog dog_t;
+
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
