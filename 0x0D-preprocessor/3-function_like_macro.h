bfa8f7941d1e61fcabe90fd3f188a249535b2145
hassan996 <compte.personnel975@gmail.com>
2023-04-17 11:03:01 -0700

    pre...

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,6 +1,10 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
+/**
+ * ABS: function d lkhra 
+ * @x:ikhan d variabl d function d lkhra
+ */
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
