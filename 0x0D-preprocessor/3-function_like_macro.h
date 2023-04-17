bab23c7f1a2acbf333d17baa96d28c7205333e71
hassan996 <compte.personnel975@gmail.com>
2023-04-17 11:06:55 -0700

    pre...

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS_H
+#define ABS_H
 
-/**
- * ABS: function d lkhra 
- * @x:ikhan d variabl d function d lkhra
- */
 #define ABS(x) ((x) < 0 ? (-x) : (x))
 
 #endif
