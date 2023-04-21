196d46c0ae63baf7ae8246c5f84196dda38abe14
hassan996 <compte.personnel975@gmail.com>
2023-04-21 08:01:43 -0700

    variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
@@ -16,12 +14,13 @@ int sum_them_all(const unsigned int n, ...)
 
 	if (n == 0)
 		return (0);
+
 	va_start(li, n);
 	for (i = 0; i < n; i++)
 	{
 		total = total + va_arg(li, int);
 	}
 	va_end(li);
-	return (sum);
-}
 
+	return (total);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function print string
+ * @separator:spearated btwn strings
+ * @n:numbers of arg passed to func
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list li;
+	unsigned int i;
+	char *s;
+
+	va_start(li, n);
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(li, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if (i < n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
