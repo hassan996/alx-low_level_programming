605d08a1a6738ba69ab5bdfccbe9afa373d5041f
hassan <compte.personnel975@gmail.com>
2023-04-27 14:28:11 -0700

    LL

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements
+ * @h:pointer to head of list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t num = 0;
+
+	while (h != NULL)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
