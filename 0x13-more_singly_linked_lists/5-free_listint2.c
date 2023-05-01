6e7d2609c9b41b5596574968b8aeb40fe8fea1a4
hassan <compte.personnel975@gmail.com>
2023-05-01 05:30:58 -0700

    MLL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function free list
+ * @head: pointer to pointer in head of list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t tmp;
+
+	if (head)
+	{
+		while (*head)
+		{
+			tmp = *head;
+			*head = tmp->next;
+			free(tmp);
+		}
+	}
+}
