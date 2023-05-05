0471527f90898dc9635b34b47acb7f8a6d87f977
hassan <compte.personnel975@gmail.com>
2023-05-04 17:11:44 -0700

    MB

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * flip_bits - function that returns the number of bits 
+ * you would need to flip to get from one number to another
+ * @n: number 1
+ * @m: number 2
+ * Return: number of bits needed to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int x = n ^ m;
+	unsigned int inc = 0;
+
+	while (x)
+	{
+		inc += x & 1;
+		x >>= 1;
+	}
+	return (inc);
+}
+
