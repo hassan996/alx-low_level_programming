fac26e4eb73f2ff66efaa397562368f8a3125233
hassan <compte.personnel975@gmail.com>
2023-05-08 13:31:34 -0700

    file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,16 +8,15 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, wr, len;
+	int op, wr, len = 0;
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content != NULL)
 	{
 		for (len = 0; text_content[len];)
-		{
 			len++;
-		}
 	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
@@ -30,4 +29,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
